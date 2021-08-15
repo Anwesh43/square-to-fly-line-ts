a5f9499d0e316bd804e9bda68a09471de46cccaa
Anwesh43 <anweshthecool0@gmail.com>
2021-08-15 06:16:25 +0530

    adding STFLNode class

--- index.ts
+++ index.ts
@@ -135,3 +135,45 @@ class Animator {
         }
     }
 }
+
+class STFLNode {
+
+    next : STFLNode 
+    prev : STFLNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new STFLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSTFLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+    
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : STFLNode {
+        var curr : STFLNode = this.prev 
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this
+    }
+}
